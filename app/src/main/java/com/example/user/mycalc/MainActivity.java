7f74e203da6496b36da3a770c85ecb2a687b9646
jk83333 <jk833@naver.com>
2016-05-28 17:31:34 +0900

    0528

---
+++ app/src/main/java/com/example/user/mycalc/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.user.mycalc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    EditText num1,num2;
+    Button btn1,btn2,btn3,btn4;
+    TextView sview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        num1 = (EditText) findViewById(R.id.num1);
+        num2 = (EditText) findViewById(R.id.num2);
+
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+
+        sview = (TextView) findViewById(R.id.sview);
+    }
+}
